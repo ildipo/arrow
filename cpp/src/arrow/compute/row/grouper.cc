33f2627a78222b8888f547888710fba70cefb6e2
Davide Pasetto <dpasetto69@gmail.com>
2023-03-08 09:22:31 -0500

    some cleanup

--- cpp/src/arrow/compute/row/grouper.cc
+++ cpp/src/arrow/compute/row/grouper.cc
@@ -24,10 +24,6 @@
 #include "arrow/array/builder_primitive.h"
 
 #include "arrow/compute/key_hash.h"
-// DIPO
-//#include "arrow/compute/exec/key_map.h"
-//#include "arrow/compute/exec/options.h"
-//#include "arrow/compute/exec_internal.h"
 #include "arrow/compute/function.h"
 #include "arrow/compute/kernels/row_encoder_internal.h"
 #include "arrow/compute/light_array.h"
